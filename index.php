61e7368565d0a8ba22ad5154ae7a12b008ba4234
rj003-ui <uon3758@gmail.com>
2025-03-01 23:24:44 +0600

    Update index.php

--- index.php
+++ index.php
@@ -1,9 +1,19 @@
 <?php
+// Get the 'get' parameter from the query string
 $get = $_GET['get'];
-$mpdUrl = $get;
 
+// Check if the URL starts with 'http://' or 'https://'
+if (!preg_match('/^https?:\/\//', $get)) {
+    // If not, prepend 'https://'
+    $mpdUrl = 'https://' . $get;
+} else {
+    // Otherwise, use the URL as is
+    $mpdUrl = $get;
+}
 
-// $context = stream_context_create($mpdheads);
+// Fetch the content of the URL
 $res = file_get_contents($mpdUrl);
+
+// Output the result
 echo $res;
 ?>
